ab99c897c4225d16a3d894ce13138370e96707fd
Ian Ganza <144003894+i-ganza007@users.noreply.github.com>
2023-10-09 15:32:11 +0300

    Update move-to-directory.sh
    By deleting >> signs

--- negpod_19-q1/move-to-directory.sh
+++ negpod_19-q1/move-to-directory.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash 
-mv main.sh Students-list_0923.txt Select-emails.sh student-emails.txt >> negpod_19-q1
+mv main.sh Students-list_0923.txt Select-emails.sh student-emails.txt  negpod_19-q1
