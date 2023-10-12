c994c46e6fcc6f07a62b649b37982b580bf3b8ef
Khalid Abdillahi <139696908+Khaalid245@users.noreply.github.com>
2023-10-12 19:55:44 +0200

    Updated main by khalid

--- negpod_19-q1/main.sh
+++ negpod_19-q1/main.sh
@@ -1,74 +1,102 @@
-#!/usr/bin/env bash 
-
-#Students record
-touch students-list_0923.txt
-
+#!/usr/bin/env bash
+# Function to create a student record
 create_student() {
-    echo "Enter student details:"
-    read -p "Student email: " email
+    echo -e "Enter student details: \n"
+    read -p "Student Email: " email
     read -p "Age: " age
     read -p "Student ID: " student_id
+    if [  -e "students-list_0923.txt" ];then
+       
+    # Check if the student ID already exists
+    if grep -q "^$student_id:" students-list_0923.txt; then
+        echo -e "Student ID $student_id already exists.\n"
+	sleep 0.5
 
-    # check if the student ID already exists
-    if grep -q "$student_id" students-list_0923.txt; then
-        echo "Student ID $student_id already exists"
+
+
+
+	
+	./main.sh
     else
-        echo "Email: $email Age: $age StudentID: $student_id" >> students-list_0923.txt
-        echo "Student record created"
+        echo "$student_id:$email:$age" >> students-list_0923.txt
+        echo "Student record created."
+	sleep 0.5
+	./main.sh
     fi
+       else
+	echo "$student_id:$email:$age" >> students-list_0923.txt
+        echo "Student record created."
+		sleep 0.5
+	./main.sh
+    fi
+       
 }
 
-#View all students records 
-view_student() {
-    echo "List of all students: "
+# Function to view all student records
+view_students() {
+    echo -e "List of all students: \n"
     cat students-list_0923.txt
+    		sleep 0.5
+	./main.sh
 }
 
-# function to delete a student record by ID
+# Function to delete a student record by ID
 delete_student() {
     read -p "Enter student ID to delete: " student_id
     if grep -q "^$student_id:" students-list_0923.txt; then
         sed -i "/^$student_id:/d" students-list_0923.txt
         echo "Student with ID $student_id deleted."
-    else 
+	
+    else
         echo "Student with ID $student_id not found."
+			sleep 0.5
+	./main.sh
     fi
 }
 
-# function to update a student record by ID
+# Function to update a student record by ID
 update_student() {
     read -p "Enter student ID to update: " student_id
     if grep -q "^$student_id:" students-list_0923.txt; then
         echo "Enter updated student details:"
-        read -p "Updated student email: " updated_email
-        read -p "Updated age: " updated_age
+        read -p "Updated Student Email: " updated_email
+        read -p "Updated Age: " updated_age
 
-        # replace the old student record with the updated one
+        # Replace the old student record with the updated one
         sed -i "s/^$student_id:.*$/$student_id:$updated_email:$updated_age/" students-list_0923.txt
         echo "Student with ID $student_id updated."
+	
     else
         echo "Student with ID $student_id not found."
+			sleep 0.5
+	./main.sh
     fi
 }
 
-# main menu 
-while true; do
-    echo "Main menu"
-    echo "1. Create student record"
-    echo "2. View students"
-    echo "3. Delete student"
-    echo "4. Update student"
+# Function to exit the application
+exit_application() {
+    echo "Exiting the application."
+    exit 0
+}
+
+# Main menu
+
+    echo -e "\nMain Menu: \n"
+    echo "1. Create Student Record"
+    echo "2. View Students"
+    echo "3. Delete Student"
+    echo "4. Update Student"
     echo "5. Select emails"
     echo "6. Exit"
-
-    read -p "Select an option (1-6): " choice 
-
+    echo -e "\n"
+    read -p "Select an option (1-6): " choice
+    echo -e "\n"
     case $choice in
         1)
             create_student
             ;;
         2)
-            view_student
+            view_students
             ;;
         3)
             delete_student
@@ -76,16 +104,17 @@ while true; do
         4)
             update_student
             ;;
-        5)
-            ./select-emails.sh
-            ;;
+	5)
+	    ./select-emails.sh
+	    ;;
         6)
-            echo "Goodbye!"
-            exit 0
+            exit_application
             ;;
         *)
+	    clear
             echo "Invalid option. Please select a valid option (1-6)."
+	    ./main.sh
             ;;
     esac
-done
+
 
